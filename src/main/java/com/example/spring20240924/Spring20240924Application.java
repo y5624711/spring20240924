f534425ab57e939b530c1d055638d377f9a87589
Yun <y5624711@gmail.com>
2024-09-24 14:06:18 +0900

    first commit

---
+++ src/main/java/com/example/spring20240924/Spring20240924Application.java
@@ -0,0 +1,13 @@
+package com.example.spring20240924;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class Spring20240924Application {
+
+    public static void main(String[] args) {
+        SpringApplication.run(Spring20240924Application.class, args);
+    }
+
+}
---
+++ src/test/java/com/example/spring20240924/Spring20240924ApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.spring20240924;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class Spring20240924ApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
