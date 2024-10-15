77885b0cbe2039f2dd46e7d0b729997ea13f9e17
Yun <y5624711@gmail.com>
2024-10-15 16:29:46 +0900

    javaBean

---
+++ src/main/java/com/example/spring20240924/dto/c28/Employees.java
@@ -0,0 +1,13 @@
+package com.example.spring20240924.dto.c28;
+
+import lombok.Data;
+
+@Data
+public class Employees {
+    private String Id;
+    private String lastName;
+    private String firstName;
+    private String birthDate;
+    private String photo;
+    private String notes;
+}
---
+++ src/main/java/com/example/spring20240924/dto/c28/Suppliers.java
@@ -0,0 +1,15 @@
+package com.example.spring20240924.dto.c28;
+
+import lombok.Data;
+
+@Data
+public class Suppliers {
+    private String id;
+    private String Name;
+    private String contactName;
+    private String address;
+    private String city;
+    private String postalCode;
+    private String country;
+    private String phone;
+}
