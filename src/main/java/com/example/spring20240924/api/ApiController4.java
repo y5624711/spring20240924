83defcd3c4caea1e211688393d67f9d48702e9aa
Yun <y5624711@gmail.com>
2024-11-12 11:28:07 +0900

    JSON path variable

---
+++ src/main/java/com/example/spring20240924/api/ApiController4.java
@@ -0,0 +1,25 @@
+package com.example.spring20240924.api;
+
+import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PathVariable;
+import org.springframework.web.bind.annotation.RequestMapping;
+
+@Controller
+@RequestMapping("/api/main4")
+public class ApiController4 {
+    @GetMapping("sub1/{name}")
+    public void sub1(@PathVariable String name) {
+        System.out.println("name = " + name);
+    }
+
+    @GetMapping("sub2/{quantity}")
+    public void sub2(@PathVariable Integer quantity) {
+        System.out.println("quantity = " + quantity);
+    }
+
+    @GetMapping("sub3/{address}")
+    public void sub3(@PathVariable String address) {
+        System.out.println("address = " + address);
+    }
+}
